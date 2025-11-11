92ea8e7bc938617eb00ae3685e2bc90520ead5dc
JeanJAKK <apotrekokoujean06@gmail.com>
2025-11-11 00:36:49 +0000

    page index et accueil

--- script.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('button');
-
-window.addEventListener('load', () => {
-    titre.classList.add("show_titre");
-    paragraphe.classList.add("show_p");
-    btn.classList.add("show_btn")
-}
